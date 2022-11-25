3fa19d70e07ea9b62f9bf2d56249656ff633b2ae
iiPython <ben@iipython.cf>
2022-11-24 23:37:48 -0600

    First push

--- src/routes.py
+++ src/routes.py
@@ -15,4 +15,15 @@ routes = web.RouteTableDef()
 async def status(req) -> web.Response:
     return mkresp(200, {"message": "OK"})
 
+@routes.post("/init")
+async def init_server(req) -> web.Response:
+    try:
+        d = await req.data()
+        print(d["gameid"], d["placever"])
+
+    except KeyError:
+        return mkresp(400, {"message": "Missing either GameID or Place Version."})
+
+    return mkresp(200, {"message": "OK"})
+
 app.add_routes(routes)
