2c5f6cad0cb6d9836bfac19b96d3b6d0b856c166
Pixlz <91509874+darkpixlz@users.noreply.github.com>
2023-01-19 10:47:30 -0500

    Add StreamX icon to footer

--- src/webhook.py
+++ src/webhook.py
@@ -18,6 +18,7 @@ server = socket.gethostname().split("-")[-1]
 # Handlers
 def upload_info(code: int, apikey: str, authkey: Union[str, None], placeid: str, placever: str, error: str) -> None:
     if not webhook_enabled:
+        print("Webhook error logging is disabled!")
         return
 
     date = datetime.now(timezone.utc).strftime("%D %H:%M UTC")
@@ -26,7 +27,7 @@ def upload_info(code: int, apikey: str, authkey: Union[str, None], placeid: str,
             {
                 "type": "rich",
                 "title": f"StreamX {server} - Code {code}",
-                "footer": {"text": f"Error code {code} generated at {date} | Powered by StreamX"},
+                "footer": {"text": f"Error code {code} generated at {date} | Powered by StreamX", "icon_url":"https://avatars.githubusercontent.com/u/117547018?s=400&u=de895b99c04ea6c7486938cbee0e424c91fc0db1&v=4"},
                 "fields": [
                     {"name": "API Key", "value": apikey, "inline": False},
                     {"name": "Place ID", "value": placeid, "inline": True},
